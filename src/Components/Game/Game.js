91bf34223ea8c032a09917fa21b0fd460c08bdff
gcnw <SkillCatcher@pm.me>
2022-09-18 07:38:24 -0700

    small progress

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Game.css';
-import GameTile from '../GameTile/GameTile'
-import xoIcon from '../../res/logo.svg'
+import GameTile from '../GameTile/GameTile';
+import xoIcon from '../../res/logo.svg';
+import xIcon from '../../res/icon-x.svg';
+import oIcon from '../../res/icon-o.svg';
+import restartIcon from '../../res/icon-restart.svg';
 
 function Game() {
   return (
     <div className='game-board'>
       <div className='info-header'>
         <img src={ xoIcon } alt='tic-tac-toe logo'></img>
+        <div id="turn-indicator">
+          <img src={ xIcon } alt='x-icon'></img><p>TURN</p>
+        </div>
+        <img src={ restartIcon } alt='restart button'></img>
       </div>
       <div className='game-tiles'>
         <GameTile />
